8b89697d1dec3d16046d84c89cdcea64c1c76262
Oluniyi Timilehin <oluniyi2004@gmail.com>
2022-09-08 08:53:40 +0100

    Initial commit

---
+++ tailwind.configr.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}", "./public/**/*.{html,js}"],
+    theme: {
+      extend: {
+        fontFamily: {
+          body: ['DM Sans']
+        }
+      },
+    },
+    plugins: [],
+  }
